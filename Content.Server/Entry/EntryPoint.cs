d4d7c6412ccd0016ba2c7642465f28299b4eb989
Aviu00 <93730715+aviu00@users.noreply.github.com>
2024-11-27 07:09:15 -0800

    Scoreboard fix 2 (#982)
    * Fix scoreboard again.
    
    * Wipe mind if no objectives.
    
    * Actually initialize ServerCurrencyManager.
    
    (cherry picked from commit e65a541371c32bdd7681c9c5944ec288605658a7)

--- Content.Server/Entry/EntryPoint.cs
+++ Content.Server/Entry/EntryPoint.cs
@@ -1,3 +1,4 @@
+using Content.Server._Goobstation.ServerCurrency;
 using Content.Server.Acz;
 using Content.Server.Administration;
 using Content.Server.Administration.Logs;
@@ -117,6 +118,7 @@ namespace Content.Server.Entry
                 _playTimeTracking.Initialize();
                 IoCManager.Resolve<JobWhitelistManager>().Initialize();
                 IoCManager.Resolve<PlayerRateLimitManager>().Initialize();
+                IoCManager.Resolve<ServerCurrencyManager>().Initialize(); // Goobstation
             }
         }
 
