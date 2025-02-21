8c64d3307e994841b977ec4870bc06aa16bbadb7
RadsammyT <radsammyt@gmail.com>
2025-02-20 23:05:38 -0500

    i did fuck something up

--- Content.Client/Guidebook/GuideEntry.cs
+++ Content.Client/Guidebook/GuideEntry.cs
@@ -34,8 +34,6 @@ public class GuideEntry
     /// </summary>
     [DataField("filterEnabled")] public bool FilterEnabled = default!;
 
-    [DataField] public bool RuleEntry;
-
     /// <summary>
     ///     This enables the ability to remove a rule entry from the guidebook.
     ///     When enabled in the yml guide entry, it will remove the guide entry from the guidebook.
